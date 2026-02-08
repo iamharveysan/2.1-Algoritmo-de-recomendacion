71d9e3ca8879ea125851b3104408b9e31014f9de
iamharveysan <137835966+iamharveysan@users.noreply.github.com>
2026-02-08 12:08:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -254,4 +254,17 @@ btnExport.addEventListener("click", () => {
   const url = URL.createObjectURL(blob);
 
   const a = document.createElement("a");
-  a.href
+  a.href = url;
+  a.download = "coursemash_votos.csv";
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+
+  URL.revokeObjectURL(url);
+});
+
+// init
+newDuel();
+renderTop();
+refreshQuestion();
+
